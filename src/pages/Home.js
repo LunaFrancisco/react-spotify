dca233ed4aab47f59ea85395aeddad3f5e873393
LunaFrancisco <francisco.luna@mail.udp.cl>
2022-12-17 19:24:45 -0300

    home view

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,34 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import { Login } from '../components/Login/login.js'
+import { LogoutButton } from '../components/Logout/index.js'
+import { SearchArtist } from '../components/SearchArtirst/index.js';
+import('./Home.css')
 
 export const Home = () => {
+
+  const [token, setToken] = useState("");
+
+  useEffect(() => {
+    const token = window.localStorage.getItem("token")
+    if (token) {
+      setToken(token)
+    }
+  }, [])
+
   return (
-    <div><h1>Spotify app</h1></div>
+    <div className='Home'>
+
+      {!token ? <Login />
+        : <>
+          <LogoutButton />
+          <h1 className='title'>Buscador de Artistas</h1>
+          <div className='home-content'>
+            <SearchArtist />
+          </div>
+
+        </>
+      }
+    </div>
+
   )
 }
